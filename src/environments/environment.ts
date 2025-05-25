d3808a930ac69ef4036720c826a2bbb4e14160b1
Samuel <daw08.2024@gmail.com>
2025-05-25 21:25:37 +0200

    Formularios mejorados y solucionado el error del crear ordenes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8000/api' 
+    apiUrl: 'http://98.66.136.54/api/api' 
+    //apiUrl: 'http://localhost:8000/api' 
   };
